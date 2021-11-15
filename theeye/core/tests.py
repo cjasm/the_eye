54d3f430bf1b56d8b7bd4d7bd3fdf2052ccf57a9
Claudio Magalhães <cjasm@cin.ufpe.br>
2021-11-15 01:46:18 -0300

    filter by session, category and time range

--- theeye/core/tests.py
+++ theeye/core/tests.py
@@ -5,21 +5,72 @@ from theeye.core.models import Event
 class EventGet(APITestCase):
 
     def setUp(self):
-        self.data = {
-            "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
-            "category": "page interaction",
-            "name": "pageview",
-            "data": {
-                "host": "www.consumeraffairs.com",
-                "path": "/",
+        self.data = [
+            {
+                "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
+                "category": "page interaction",
+                "name": "pageview",
+                "data": {
+                    "host": "www.consumeraffairs.com",
+                    "path": "/",
+                },
+                "timestamp": "2021-01-01 09:15:27.243860"
             },
-            "timestamp": "2021-01-01 09:15:27.243860"
-        }
-        Event.objects.create(**self.data)
+            {
+                "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
+                "category": "page interaction",
+                "name": "cta click",
+                "data": {
+                    "host": "www.consumeraffairs.com",
+                    "path": "/",
+                    "element": "chat bubble"
+                },
+                "timestamp": "2021-01-01 09:15:27.243860"
+            },
+            {
+                "session_id": "e2085be5-9137-4e4e-80b5-f1ffddc25423",
+                "category": "form interaction",
+                "name": "submit",
+                "data": {
+                    "host": "www.consumeraffairs.com",
+                    "path": "/",
+                    "form": {
+                        "first_name": "John",
+                        "last_name": "Doe"
+                    }
+                },
+                "timestamp": "2021-01-01 09:15:27.243860"
+            },
+            {
+                "session_id": "aaaaaa-aaaa-aaaaaa-aaaa",
+                "category": "page interaction",
+                "name": "cta click",
+                "data": {
+                    "host": "www.consumeraffairs.com",
+                    "path": "/",
+                    "element": "chat bubble"
+                },
+                "timestamp": "2021-01-02 09:15:27.243860"
+            }
+        ]
+        for event in self.data:
+            Event.objects.create(**event)
         self.resp = self.client.get('/api/events/')
 
     def test_get(self):
         self.assertEqual(self.resp.status_code, 200)
 
     def test_has_event(self):
-        self.assertEqual(len(self.resp.data), 1)
+        self.assertEqual(len(self.resp.data), 4)
+
+    def test_filter_by_category(self):
+        response = self.client.get('/api/events/?category=form%20interaction')
+        self.assertEqual(len(response.data), 1)
+
+    def test_filter_by_time_range(self):
+        response = self.client.get('/api/events/?date-from=2021-01-01 09:15:27.243860&date-to=2021-01-01 09:15:27.243860')
+        self.assertEqual(len(response.data), 3)
+
+    def test_filter_by_category(self):
+        response = self.client.get('/api/events/?session=aaaaaa-aaaa-aaaaaa-aaaa')
+        self.assertEqual(len(response.data), 1)
--- theeye/core/views.py
+++ theeye/core/views.py
@@ -1,3 +1,4 @@
+from django.utils.dateparse import parse_datetime
 from rest_framework import viewsets
 from rest_framework.response import Response
 from theeye.core.models import Event
@@ -11,5 +12,18 @@ class EventViewSet(viewsets.ViewSet):
 
     def list(self, request):
         queryset = Event.objects.all()
+        date_from_query = request.query_params.get('date-from', None)
+        date_to_query = request.query_params.get('date-to', None)
+        category_query = request.query_params.get('category', None)
+        session_query = request.query_params.get('session', None)
+
+        if date_from_query and date_to_query:
+            date_from_query, date_to_query = parse_datetime(date_from_query), parse_datetime(date_to_query)
+            queryset = queryset.filter(timestamp__range=(date_from_query, date_to_query))
+        if category_query:
+            queryset = queryset.filter(category=category_query)
+        if session_query:
+            queryset = queryset.filter(session_id=session_query)
+
         serializer = EventSerializer(queryset, many=True)
         return Response(serializer.data)
